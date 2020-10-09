36c73a29e3b7101cf24c4953331e75e9e87e1588
mc4117 <mc4117@imperial.ac.uk>
2020-10-09 13:48:16 +0100

    clean up

---
+++ neural_notebooks/python_files/seasonal_nn_72.py
@@ -0,0 +1,240 @@
+import numpy as np
+import xarray as xr
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras.layers import *
+import tensorflow.keras.backend as K
+from src.score import *
+import re
+
+device_name = tf.test.gpu_device_name()
+if device_name != '/device:GPU:0':
+    raise SystemError('GPU device not found')
+print('Found GPU at: {}'.format(device_name))
+
+def limit_mem():
+    """By default TF uses all available GPU memory. This function prevents this."""
+    config = tf.compat.v1.ConfigProto()
+    config.gpu_options.allow_growth = True
+    tf.compat.v1.Session(config=config)
+
+class DataGenerator(keras.utils.Sequence):
+    def __init__(self, ds, var_dict, lead_time, batch_size=32, shuffle=True, load=True, 
+                 mean=None, std=None, output_vars=None):
+        """
+        Data generator for WeatherBench data.
+        Template from https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+        Args:
+            ds: Dataset containing all variables
+            var_dict: Dictionary of the form {'var': level}. Use None for level if data is of single level
+            lead_time: Lead time in hours
+            batch_size: Batch size
+            shuffle: bool. If True, data is shuffled.
+            load: bool. If True, datadet is loaded into RAM.
+            mean: If None, compute mean from data.
+            std: If None, compute standard deviation from data.
+        """
+
+        self.ds = ds
+        self.var_dict = var_dict
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.lead_time = lead_time
+
+        data = []
+        level_names = []
+        generic_level = xr.DataArray([1], coords={'level': [1]}, dims=['level'])
+        for long_var, params in var_dict.items():
+            if long_var == 'constants': 
+                for var in params:
+                    data.append(ds[var].expand_dims(
+                        {'level': generic_level, 'time': ds.time}, (1, 0)
+                    ))
+                    level_names.append(var)
+            else:
+                var, levels = params
+                try:
+                    data.append(ds[var].sel(level=levels))
+                    level_names += [f'{var}_{level}' for level in levels]
+                except ValueError:
+                    data.append(ds[var].expand_dims({'level': generic_level}, 1))
+                    level_names.append(var)
+
+        self.data = xr.concat(data, 'level').transpose('time', 'lat', 'lon', 'level')
+        self.data['level_names'] = xr.DataArray(
+            level_names, dims=['level'], coords={'level': self.data.level})
+        if output_vars is None:
+            self.output_idxs = range(len(dg_valid.data.level))
+        else:
+            self.output_idxs = [i for i, l in enumerate(self.data.level_names.values) 
+                                if any([bool(re.match(o, l)) for o in output_vars])]
+        
+        # Normalize
+        self.mean = self.data.mean(('time', 'lat', 'lon')).compute() if mean is None else mean
+#         self.std = self.data.std('time').mean(('lat', 'lon')).compute() if std is None else std
+        self.std = self.data.std(('time', 'lat', 'lon')).compute() if std is None else std
+        self.data = (self.data - self.mean) / self.std
+        
+        self.n_samples = self.data.isel(time=slice(0, -lead_time)).shape[0]
+        self.init_time = self.data.isel(time=slice(None, -lead_time)).time
+        self.valid_time = self.data.isel(time=slice(lead_time, None)).time
+
+        self.on_epoch_end()
+
+        # For some weird reason calling .load() earlier messes up the mean and std computations
+        if load: print('Loading data into RAM'); self.data.load()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.ceil(self.n_samples / self.batch_size))
+
+    def __getitem__(self, i):
+        'Generate one batch of data'
+        idxs = self.idxs[i * self.batch_size:(i + 1) * self.batch_size]
+        X = self.data.isel(time=idxs).values
+        y = self.data.isel(time=idxs + self.lead_time, level=self.output_idxs).values
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.idxs = np.arange(self.n_samples)
+        if self.shuffle == True:
+            np.random.shuffle(self.idxs)
+    
+limit_mem()
+
+DATADIR = '/rds/general/user/mc4117/home/WeatherBench/data/'
+
+var_dict = {
+    'geopotential': ('z', [500]),
+    'temperature': ('t', [850]),
+    'specific_humidity': ('q', [500, 850]),
+    '2m_temperature': ('t2m', None),
+    'toa_incident_solar_radiation': ('tisr', None),
+    'potential_vorticity': ('pv', [500, 850]),
+    
+    'constants': ['lat2d', 'orography', 'lsm']
+}
+
+ds = [xr.open_mfdataset(f'{DATADIR}/{var}/*.nc', combine='by_coords') for var in var_dict.keys()]
+
+ds_whole = xr.merge(ds)
+
+ds_2015_06 = ds_whole.sel(time = '2015-06')
+ds_2015_07 = ds_whole.sel(time = '2015-07')
+ds_2015_08 = ds_whole.sel(time = '2015-08')
+ds_2016_06 = ds_whole.sel(time = '2016-06')
+ds_2016_07 = ds_whole.sel(time = '2016-07')
+ds_2016_08 = ds_whole.sel(time = '2016-08')
+ds_2017_06 = ds_whole.sel(time = '2017-06')
+ds_2017_07 = ds_whole.sel(time = '2017-07')
+ds_2017_08 = ds_whole.sel(time = '2017-08')
+ds_2018_06 = ds_whole.sel(time = '2018-06')
+ds_2018_07 = ds_whole.sel(time = '2018-07')
+ds_2018_08 = ds_whole.sel(time = '2018-08')
+
+
+ds_train = xr.merge([ds_2015_06, ds_2015_07, ds_2015_08])
+ds_valid = xr.merge([ds_2016_06, ds_2016_07, ds_2016_08])
+ds_test = xr.merge([ds_2017_06, ds_2017_07, ds_2017_08, ds_2018_06, ds_2018_07, ds_2018_08])
+
+bs=32
+lead_time=72
+output_vars = ['z_500', 't_850']
+
+# Create a training and validation data generator. Use the train mean and std for validation as well.
+dg_train = DataGenerator(ds_train, var_dict, lead_time, batch_size=bs, load=True, 
+                         output_vars=output_vars)
+dg_valid = DataGenerator(ds_valid, var_dict, lead_time, batch_size=bs, mean=dg_train.mean, std=dg_train.std, 
+                         shuffle=False, output_vars=output_vars)
+
+dg_test = DataGenerator(ds_test, var_dict, lead_time, batch_size=bs, mean=dg_train.mean, std=dg_train.std, 
+                         shuffle=False, output_vars=output_vars)
+
+X, y = dg_train[0]; 
+
+print(X.shape)
+print(y.shape)
+
+class PeriodicPadding2D(tf.keras.layers.Layer):
+    def __init__(self, pad_width, **kwargs):
+        super().__init__(**kwargs)
+        self.pad_width = pad_width
+
+    def call(self, inputs, **kwargs):
+        if self.pad_width == 0:
+            return inputs
+        inputs_padded = tf.concat(
+            [inputs[:, :, -self.pad_width:, :], inputs, inputs[:, :, :self.pad_width, :]], axis=2)
+        # Zero padding in the lat direction
+        inputs_padded = tf.pad(inputs_padded, [[0, 0], [self.pad_width, self.pad_width], [0, 0], [0, 0]])
+        return inputs_padded
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'pad_width': self.pad_width})
+        return config
+
+
+class PeriodicConv2D(tf.keras.layers.Layer):
+    def __init__(self, filters,
+                 kernel_size,
+                 conv_kwargs={},
+                 **kwargs, ):
+        super().__init__(**kwargs)
+        self.filters = filters
+        self.kernel_size = kernel_size
+        self.conv_kwargs = conv_kwargs
+        if type(kernel_size) is not int:
+            assert kernel_size[0] == kernel_size[1], 'PeriodicConv2D only works for square kernels'
+            kernel_size = kernel_size[0]
+        pad_width = (kernel_size - 1) // 2
+        self.padding = PeriodicPadding2D(pad_width)
+        self.conv = Conv2D(
+            filters, kernel_size, padding='valid', **conv_kwargs
+        )
+
+    def call(self, inputs):
+        return self.conv(self.padding(inputs))
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({'filters': self.filters, 'kernel_size': self.kernel_size, 'conv_kwargs': self.conv_kwargs})
+        return config
+    
+def build_cnn(filters, kernels, input_shape, dr=0):
+    """Fully convolutional network"""
+    x = input = Input(shape=input_shape)
+    for f, k in zip(filters[:-1], kernels[:-1]):
+        x = PeriodicConv2D(f, k)(x)
+        x = LeakyReLU()(x)
+        x = BatchNormalization()(x)
+        if dr > 0: x = Dropout(dr)(x, training = True)
+    output = PeriodicConv2D(filters[-1], kernels[-1])(x)
+    return keras.models.Model(input, output)
+
+cnn = build_cnn([64, 64, 64, 64, 2], [5, 5, 5, 5, 5], (32, 64, 11), dr = 0.1)
+
+cnn.compile(keras.optimizers.Adam(1e-4), 'mse')
+
+print(cnn.summary())
+
+checkpoint_filepath = '/rds/general/user/mc4117/home/WeatherBench/checkpoint/'
+model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_filepath,
+    save_weights_only=True,
+    monitor='val_loss',
+    mode='min',
+    save_best_only=True)
+
+cnn.fit(x = dg_train, epochs=100, validation_data=dg_valid, 
+          callbacks=[tf.keras.callbacks.EarlyStopping(
+                        monitor='val_loss',
+                        min_delta=0,
+                        patience=5,
+                        verbose=1, 
+                        mode='auto'
+                    ), model_checkpoint_callback]
+         )
+
+cnn.save_weights('/rds/general/user/mc4117/home/WeatherBench/saved_models/train_72_multi_data_gpu_seasonal.h5')
