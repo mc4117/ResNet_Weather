f36d26040e062d24aa222ecd7a02aff811cda6b6
mc4117 <mc4117@imperial.ac.uk>
2020-10-19 15:25:15 +0100

    starting to look at residual nets

--- neural_notebooks/python_files/multivariable/.ipynb_checkpoints/generate_data-checkpoint.py
+++ neural_notebooks/python_files/multivariable/.ipynb_checkpoints/generate_data-checkpoint.py
@@ -90,6 +90,87 @@ class DataGenerator(keras.utils.Sequence):
         self.idxs = np.arange(self.n_samples)
         if self.shuffle == True:
             np.random.shuffle(self.idxs)
+            
+class DataGeneratormaxmin(keras.utils.Sequence):
+    def __init__(self, ds, var_dict, lead_time, batch_size=32, shuffle=True, load=True, 
+                 max_data=None, min_data=None, output_vars=None):
+        """
+        Data generator for WeatherBench data.
+        Template from https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+        Args:
+            ds: Dataset containing all variables
+            var_dict: Dictionary of the form {'var': level}. Use None for level if data is of single level
+            lead_time: Lead time in hours
+            batch_size: Batch size
+            shuffle: bool. If True, data is shuffled.
+            load: bool. If True, datadet is loaded into RAM.
+            max_data: If None, compute max_data from data.
+            min_data: If None, compute min_data from data.
+        """
+
+        self.ds = ds
+        self.var_dict = var_dict
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.lead_time = lead_time
+
+        data = []
+        level_names = []
+        generic_level = xr.DataArray([1], coords={'level': [1]}, dims=['level'])
+        for long_var, params in var_dict.items():
+            if long_var == 'constants': 
+                for var in params:
+                    data.append(ds[var].expand_dims(
+                        {'level': generic_level, 'time': ds.time}, (1, 0)
+                    ))
+                    level_names.append(var)
+            else:
+                var, levels = params
+                try:
+                    data.append(ds[var].sel(level=levels))
+                    level_names += [f'{var}_{level}' for level in levels]
+                except ValueError:
+                    data.append(ds[var].expand_dims({'level': generic_level}, 1))
+                    level_names.append(var)
+
+        self.data = xr.concat(data, 'level').transpose('time', 'lat', 'lon', 'level')
+        self.data['level_names'] = xr.DataArray(
+            level_names, dims=['level'], coords={'level': self.data.level})
+        if output_vars is None:
+            self.output_idxs = range(len(dg_valid.data.level))
+        else:
+            self.output_idxs = [i for i, l in enumerate(self.data.level_names.values) 
+                                if any([bool(re.match(o, l)) for o in output_vars])]
+        
+        # Max min scalar
+        self.max_data = self.data.max(('time', 'lat', 'lon')).compute() if max_data is None else max_data
+        self.min_data = self.data.min(('time', 'lat', 'lon')).compute() if min_data is None else min_data
+        self.data = (self.data - self.min_data) / (self.max_data - self.min_data)
+        
+        self.n_samples = self.data.isel(time=slice(0, -lead_time)).shape[0]
+        self.init_time = self.data.isel(time=slice(None, -lead_time)).time
+        self.valid_time = self.data.isel(time=slice(lead_time, None)).time
+
+        self.on_epoch_end()
+
+        if load: print('Loading data into RAM'); self.data.load()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.ceil(self.n_samples / self.batch_size))
+
+    def __getitem__(self, i):
+        'Generate one batch of data'
+        idxs = self.idxs[i * self.batch_size:(i + 1) * self.batch_size]
+        X = self.data.isel(time=idxs).values
+        y = self.data.isel(time=idxs + self.lead_time, level=self.output_idxs).values
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.idxs = np.arange(self.n_samples)
+        if self.shuffle == True:
+            np.random.shuffle(self.idxs)            
 
 def create_data(var_name):
     DATADIR = '/rds/general/user/mc4117/home/WeatherBench/data/'
@@ -143,6 +224,118 @@ def create_data(var_name):
                          shuffle=False, output_vars=output_vars)
 
     dg_test = DataGenerator(ds_test, var_dict, lead_time, batch_size=bs, mean=dg_train.mean, std=dg_train.std, 
+                         shuffle=False, output_vars=output_vars)
+    
+    
+    return dg_train, dg_valid, dg_test
+
+
+def create_data_second_test(var_name):
+    DATADIR = '/rds/general/user/mc4117/home/WeatherBench/data/'
+
+    if var_name == 'specific_humidity':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'specific_humidity': ('q', [500, 850])}
+    elif var_name == '2m temp':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            '2m_temperature': ('t2m', None)}
+    elif var_name == 'solar rad':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'toa_incident_solar_radiation': ('tisr', None)}
+    elif var_name == 'pot_vort':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'potential_vorticity': ('pv', [500, 850])}
+    elif var_name == 'const':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'constants': ['lat2d', 'orography', 'lsm']}
+    elif var_name == 'orig':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850])} 
+
+    ds = [xr.open_mfdataset(f'{DATADIR}/{var}/*.nc', combine='by_coords') for var in var_dict.keys()]
+
+    ds_whole = xr.merge(ds)
+
+    ds_test2 = ds_whole.sel(time=slice('2013', '2014'))
+    ds_train = ds_whole.sel(time=slice('2015', '2015'))
+    
+    bs=32
+    lead_time=72
+    output_vars = ['z_500', 't_850']
+
+    # Create a training and validation data generator. Use the train mean and std for validation as well.
+    dg_train = DataGenerator(ds_train, var_dict, lead_time, batch_size=bs, load=True, 
+                         output_vars=output_vars)    
+    dg_test2 = DataGenerator(ds_test2, var_dict, lead_time, batch_size=bs, mean=dg_train.mean, std=dg_train.std, 
+                         shuffle=False, output_vars=output_vars)
+    
+    
+    return dg_test2
+
+
+def create_data_max_min(var_name):
+    DATADIR = '/rds/general/user/mc4117/home/WeatherBench/data/'
+
+    if var_name == 'specific_humidity':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'specific_humidity': ('q', [500, 850])}
+    elif var_name == '2m temp':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            '2m_temperature': ('t2m', None)}
+    elif var_name == 'solar rad':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'toa_incident_solar_radiation': ('tisr', None)}
+    elif var_name == 'pot_vort':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'potential_vorticity': ('pv', [500, 850])}
+    elif var_name == 'const':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850]),
+            'constants': ['lat2d', 'orography', 'lsm']}
+    elif var_name == 'orig':
+        var_dict = {
+            'geopotential': ('z', [500]),
+            'temperature': ('t', [850])} 
+
+    ds = [xr.open_mfdataset(f'{DATADIR}/{var}/*.nc', combine='by_coords') for var in var_dict.keys()]
+
+    ds_whole = xr.merge(ds)
+
+    ds_train = ds_whole.sel(time=slice('2015', '2015'))
+    ds_valid = ds_whole.sel(time=slice('2016', '2016'))
+    ds_test = ds_whole.sel(time=slice('2017', '2018'))
+    
+    bs=32
+    lead_time=72
+    output_vars = ['z_500', 't_850']
+
+    # Create a training and validation data generator. Use the train mean and std for validation as well.
+    dg_train = DataGeneratormaxmin(ds_train, var_dict, lead_time, batch_size=bs, load=True, 
+                         output_vars=output_vars)
+    dg_valid = DataGeneratormaxmin(ds_valid, var_dict, lead_time, batch_size=bs, max_data=dg_train.max_data, min_data=dg_train.min_data, 
+                         shuffle=False, output_vars=output_vars)
+
+    dg_test = DataGeneratormaxmin(ds_test, var_dict, lead_time, batch_size=bs, max_data=dg_train.max_data, min_data=dg_train.min_data,
                          shuffle=False, output_vars=output_vars)
     
     
--- neural_notebooks/python_files/multivariable/annual_nn_72_minmax.py
+++ neural_notebooks/python_files/multivariable/annual_nn_72_minmax.py
@@ -6,52 +6,22 @@ from tensorflow.keras.layers import *
 import tensorflow.keras.backend as K
 from src.score import *
 import re
-from collections import OrderedDict
+
+import sys
+print("Script name ", sys.argv[0])
+
+var_name = sys.argv[1]
+
+print(var_name)
 
 device_name = tf.test.gpu_device_name()
 if device_name != '/device:GPU:0':
     raise SystemError('GPU device not found')
 print('Found GPU at: {}'.format(device_name))
 
-DATADIR = '/rds/general/user/mc4117/home/WeatherBench/data/'
-
-# For the data generator all variables have to be merged into a single dataset.
-var_dict = {
-    'geopotential': ('z', [500, 850]),
-    'temperature': ('t', [500, 850]),
-    'specific_humidity': ('q', [850]),
-    '2m_temperature': ('t2m', None),
-    'potential_vorticity': ('pv', [50, 100]),
-    'constants': ['lsm', 'orography']
-}
-
-ds_list = []
-
-for long_var, params in var_dict.items():
-    if long_var == 'constants':
-        ds_list.append(xr.open_mfdataset(f'{DATADIR}/{long_var}/*.nc', combine='by_coords'))
-    else:
-        var, levels = params
-        if levels is not None:
-            ds_list.append(xr.open_mfdataset(f'{DATADIR}/{long_var}/*.nc', combine='by_coords').sel(level = levels))
-        else:
-            ds_list.append(xr.open_mfdataset(f'{DATADIR}/{long_var}/*.nc', combine='by_coords'))
-
-print('got here')
-
-ds_whole = xr.merge(ds_list)
-
-del ds_list
-
-# In this notebook let's only load a subset of the training data
-ds_train = ds_whole.sel(time=slice('1979', '2016'))  
-ds_test = ds_whole.sel(time=slice('2017', '2018'))
-
-del ds_whole
-
 class DataGenerator(keras.utils.Sequence):
     def __init__(self, ds, var_dict, lead_time, batch_size=32, shuffle=True, load=True, 
-                 mean=None, std=None, output_vars=None):
+                 max_data=None, min_data=None, output_vars=None):
         """
         Data generator for WeatherBench data.
         Template from https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
@@ -62,8 +32,8 @@ class DataGenerator(keras.utils.Sequence):
             batch_size: Batch size
             shuffle: bool. If True, data is shuffled.
             load: bool. If True, datadet is loaded into RAM.
-            mean: If None, compute mean from data.
-            std: If None, compute standard deviation from data.
+            max_data: If None, compute max_data from data.
+            min_data: If None, compute min_data from data.
         """
 
         self.ds = ds
@@ -100,11 +70,10 @@ class DataGenerator(keras.utils.Sequence):
             self.output_idxs = [i for i, l in enumerate(self.data.level_names.values) 
                                 if any([bool(re.match(o, l)) for o in output_vars])]
         
-        # Normalize
-        self.mean = self.data.mean(('time', 'lat', 'lon')).compute() if mean is None else mean
-#         self.std = self.data.std('time').mean(('lat', 'lon')).compute() if std is None else std
-        self.std = self.data.std(('time', 'lat', 'lon')).compute() if std is None else std
-        self.data = (self.data - self.mean) / self.std
+        # Max min scalar
+        self.max_data = self.data.max(('time', 'lat', 'lon')).compute() if max_data is None else max_data
+        self.min_data = self.data.min(('time', 'lat', 'lon')).compute() if min_data is None else min_data
+        self.data = (self.data - self.min_data) / (self.max_data - self.min_data)
         
         self.n_samples = self.data.isel(time=slice(0, -lead_time)).shape[0]
         self.init_time = self.data.isel(time=slice(None, -lead_time)).time
@@ -112,7 +81,6 @@ class DataGenerator(keras.utils.Sequence):
 
         self.on_epoch_end()
 
-        # For some weird reason calling .load() earlier messes up the mean and std computations
         if load: print('Loading data into RAM'); self.data.load()
 
     def __len__(self):
@@ -132,19 +100,63 @@ class DataGenerator(keras.utils.Sequence):
         if self.shuffle == True:
             np.random.shuffle(self.idxs)
 
+DATADIR = '/rds/general/user/mc4117/home/WeatherBench/data/'
+
+if var_name == 'specific_humidity':
+    var_dict = {
+        'geopotential': ('z', [500]),
+        'temperature': ('t', [850]),
+        'specific_humidity': ('q', [500, 850])}
+elif var_name == '2m temp':
+    var_dict = {
+        'geopotential': ('z', [500]),
+        'temperature': ('t', [850]),
+        '2m_temperature': ('t2m', None)}
+elif var_name == 'solar rad':
+    var_dict = {
+        'geopotential': ('z', [500]),
+        'temperature': ('t', [850]),
+        'toa_incident_solar_radiation': ('tisr', None)}
+elif var_name == 'pot_vort':
+    var_dict = {
+        'geopotential': ('z', [500]),
+        'temperature': ('t', [850]),
+        'potential_vorticity': ('pv', [500, 850])}
+elif var_name == 'const':
+    var_dict = {
+        'geopotential': ('z', [500]),
+        'temperature': ('t', [850]),
+        'constants': ['lat2d', 'orography', 'lsm']}
+elif var_name == 'orig':
+    var_dict = {
+        'geopotential': ('z', [500]),
+        'temperature': ('t', [850])} 
+
+ds = [xr.open_mfdataset(f'{DATADIR}/{var}/*.nc', combine='by_coords') for var in var_dict.keys()]
+
+ds_whole = xr.merge(ds)
+
+ds_train = ds_whole.sel(time=slice('2015', '2015'))
+ds_valid = ds_whole.sel(time=slice('2016', '2016'))
+ds_test = ds_whole.sel(time=slice('2017', '2018'))
+
 bs=32
 lead_time=72
 output_vars = ['z_500', 't_850']
 
-# Create a training and validation data generator. Use the train mean and std for validation as well.
-dg_train = DataGenerator(
-    ds_train.sel(time=slice('1979', '2015')), var_dict, lead_time, batch_size=bs, load=True, output_vars = output_vars)
-dg_valid = DataGenerator(
-    ds_train.sel(time=slice('2016', '2016')), var_dict, lead_time, batch_size=bs, mean=dg_train.mean, std=dg_train.std, shuffle=False, output_vars = output_vars)
-
-# Now also a generator for testing. Impartant: Shuffle must be False!
-dg_test = DataGenerator(ds_test, var_dict, lead_time, batch_size=bs, mean=dg_train.mean, std=dg_train.std, 
+# Create a training and validation data generator. Use the train max and min for validation as well.
+dg_train = DataGenerator(ds_train, var_dict, lead_time, batch_size=bs, load=True, 
+                         output_vars=output_vars)
+dg_valid = DataGenerator(ds_valid, var_dict, lead_time, batch_size=bs, max_data=dg_train.max_data, min_data=dg_train.min_data, 
                          shuffle=False, output_vars=output_vars)
+
+dg_test = DataGenerator(ds_test, var_dict, lead_time, batch_size=bs, max_data=dg_train.max_data, min_data=dg_train.min_data, 
+                         shuffle=False, output_vars=output_vars)
+
+X, y = dg_train[0]; 
+
+print(X.shape)
+print(y.shape)
 
 class PeriodicPadding2D(tf.keras.layers.Layer):
     def __init__(self, pad_width, **kwargs):
@@ -192,78 +204,40 @@ class PeriodicConv2D(tf.keras.layers.Layer):
         config.update({'filters': self.filters, 'kernel_size': self.kernel_size, 'conv_kwargs': self.conv_kwargs})
         return config
     
-def create_predictions(model, dg):
-    """Create non-iterative predictions"""
-    preds = xr.DataArray(
-        model.predict_generator(dg),
-        dims=['time', 'lat', 'lon', 'level'],
-        coords={'time': dg.valid_time, 'lat': dg.data.lat, 'lon': dg.data.lon, 
-                'level': dg.data.isel(level=dg.output_idxs).level,
-                'level_names': dg.data.isel(level=dg.output_idxs).level_names
-               },
-    )
-    # Unnormalize
-    preds = (preds * dg.std.isel(level=dg.output_idxs).values + 
-             dg.mean.isel(level=dg.output_idxs).values)
-    unique_vars = list(set([l.split('_')[0] for l in preds.level_names.values])); unique_vars
-    
-    das = []
-    for v in unique_vars:
-        idxs = [i for i, vv in enumerate(preds.level_names.values) if vv.split('_')[0] in v]
-        #print(v, idxs)
-        da = preds.isel(level=idxs).squeeze().drop('level_names')
-        if not 'level' in da.dims: da.drop('level')
-        das.append({v: da})
-    return xr.merge(das, compat = 'override').drop('level')
-
 def build_cnn(filters, kernels, input_shape, dr=0):
     """Fully convolutional network"""
     x = input = Input(shape=input_shape)
     for f, k in zip(filters[:-1], kernels[:-1]):
         x = PeriodicConv2D(f, k)(x)
+        #x = Activation(activation='sigmoid')(x)
         x = LeakyReLU()(x)
-        # x = BatchNormalization()(x)
     output = PeriodicConv2D(filters[-1], kernels[-1])(x)
     return keras.models.Model(input, output)
 
-checkpoint_filepath = '/rds/general/user/mc4117/home/WeatherBench/checkpoint2/'
-model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath=checkpoint_filepath,
-    save_weights_only=True,
-    monitor='val_loss',
-    mode='min',
-    save_best_only=True)
-    
-for i in range(4):
-    cnn = build_cnn([64, 64, 64, 64, 2], [5, 5, 5, 5, 5], (32, 64, 10))
+if var_name == 'specific_humidity':
+    cnn = build_cnn([64, 64, 64, 64, 2], [5, 5, 5, 5, 5], (32, 64, 4))
+elif var_name == 'pot_vort':
+    cnn = build_cnn([64, 64, 64, 64, 2], [5, 5, 5, 5, 5], (32, 64, 4))
+elif var_name == 'const':
+    cnn = build_cnn([64, 64, 64, 64, 2], [5, 5, 5, 5, 5], (32, 64, 5))
+elif var_name == 'orig':
+    cnn = build_cnn([64, 64, 64, 64, 2], [5, 5, 5, 5, 5], (32, 64, 2))
+else:
+    cnn = build_cnn([64, 64, 64, 64, 2], [5, 5, 5, 5, 5], (32, 64, 3))
 
-    cnn.compile(keras.optimizers.Adam(1e-4), 'mse')
+cnn.compile(keras.optimizers.Adam(1e-4), 'mse')
 
-    print(cnn.summary())
+print(cnn.summary())
 
-    cnn.fit(x = dg_train, epochs=100, validation_data=dg_valid, 
-          callbacks=[tf.keras.callbacks.EarlyStopping(
-                        monitor='val_loss',
-                        min_delta=0,
-                        patience=2,
-                        verbose=1, 
-                        mode='auto'
-                    ), model_checkpoint_callback]
-         )
-    filename = '/rds/general/user/mc4117/ephemeral/saved_models/whole_mm_more_data_' + str(i)
-    cnn.save_weights(filename + '.h5')    
 
-    number_of_forecasts = 1
+def scheduler(epoch, lr):
+    if epoch < 5:
+        return lr
+    else:
+        return lr * tf.math.exp(-0.1)
 
-    pred_ensemble=np.ndarray(shape=(2, 17448, 32, 64, number_of_forecasts),dtype=np.float32)
-    print(pred_ensemble.shape)
-    forecast_counter=np.zeros(number_of_forecasts,dtype=int)
+callback = tf.keras.callbacks.LearningRateScheduler(scheduler)    
 
-    for j in range(number_of_forecasts):
-        print(j)
-        output = create_predictions(cnn, dg_test)
-        pred2 = np.asarray(output.to_array(), dtype=np.float32).squeeze()
-        pred_ensemble[:,:,:,:,j]=pred2
-        forecast_counter[j]=j+1
-        filename_2 = '/rds/general/user/mc4117/ephemeral/saved_pred/whole_mm_more_data_' + str(i)
-        np.save(filename_2 + '.npy', pred_ensemble)
+cnn.fit(x = dg_train, epochs=30, validation_data=dg_valid, callbacks=[callback])
+
+cnn.save_weights('/rds/general/user/mc4117/home/WeatherBench/saved_models/72_gpu_annual_maxmin' + str(var_name) + '.h5')
